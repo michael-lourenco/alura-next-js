206814f639681e37f5f736fb21cf55bd8e54a895
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-11 14:14:02 -0300

    Como lidar com estilos globais

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
-import Link from "../src/components/Link"
+import Link from "../src/components/Link";
+
+function Title({ children,  as }) {
+  const Tag = as;
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>
+        {`
+          ${Tag} {
+            color: #e0e0e0;
+            font-family: sans-serif;
+          }
+        `}
+      </style>
+    </>
+  );
+}
 
 function HomePage() {
   return (
     <>
-    <div>Welcome to Next.js!</div>
-    <Link href="/faq" > ir para o faq</Link>
+      <Title as="h1">Welcome to Next.js!</Title>
+      <Link href="/faq"> ir para o faq</Link>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,13 @@
+export default function GlobalStyle() {
+  return (
+      <style global jsx>
+        {`
+          body {
+            font-family: sans-serif;
+            background-color: #101010;
+            color: #f0f0f0;
+          }
+        `}
+      </style>
+  )
+}
