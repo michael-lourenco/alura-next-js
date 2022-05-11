1e821408db7a9ffc7ff90af72b09ce9836816b81
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-11 15:06:12 -0300

    Componentes do layout

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children,  as }) {
-  const Tag = as;
-  return (
-    <>
-      <Tag>{children}</Tag>
-      <style jsx>
-        {`
-          ${Tag} {
-            color: #e0e0e0;
-            font-family: sans-serif;
-          }
-        `}
-      </style>
-    </>
-  );
-}
+export default HomeScreen;
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
 
-function HomePage() {
-  return (
-    <>
-      <Title as="h1">Welcome to Next.js!</Title>
-      <Link href="/faq"> ir para o faq</Link>
-    </>
-  );
-}
+// function Title({ children,  as }) {
+//   const Tag = as;
+//   return (
+//     <>
+//       <Tag>{children}</Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: #e0e0e0;
+//             font-family: sans-serif;
+//           }
+//         `}
+//       </style>
+//     </>
+//   );
+// }
 
-export default HomePage;
+// function HomePage() {
+//   return (
+//     <>
+//       <Title as="h1">Welcome to Next.js!</Title>
+//       <Link href="/faq"> ir para o faq</Link>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default HomePage;
