9624dbf1969a1de19770af98f902c384a2b40cfb
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-05-18 08:53:30 -0300

    getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,11 @@
 import Link from "../src/components/Link"
 
+// console.log('Em modo dev ambos sempre rodam a cada acesso')
+
+//export async function getServerSideProps() {
+// console.log('Roda a cada acesso que você recebe')
 export async function getStaticProps() {
+// console.log('Roda somente em build time')
   const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
   const faq = await fetch(FAQ_API_URL)
     .then(response => response.json())
